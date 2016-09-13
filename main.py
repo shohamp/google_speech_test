45a2244280031b7280ee379df1193a4bcc4a05af
Shoham Peller <shoham.peller@stratoscale.com>
2016-09-13 23:24:35 +0300

    Added the script itself

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import subprocess
+
+TOKEN = sys.argv[1]
+HERE = os.path.dirname(__file__)
+json_path = os.path.join(HERE, "sync-request.json")
+
+success = 0
+for i in xrange(100):
+    print "Running attempt %d   \r" % (i + 1),
+    sys.stdout.flush()
+    result = subprocess.check_output('curl -s -k -H "Content-Type: application/json" '
+                                  '-H "Authorization: Bearer %s" https://speech.googleapis.'
+                                  'com/v1beta1/speech:syncrecognize -d @%s'
+                                  % (TOKEN, json_path), shell=True)
+    if 'how old is the Brooklyn Bridge' in result:
+        success += 1
+
+print
+print "Succeeded %d times out of 100" % success
